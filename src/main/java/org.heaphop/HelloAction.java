0a54715c087b0c5b3b631618fe50a359ffe57959
Sirbu Alexandru-Gabriel <sirbualexandrugabriel@gmail.com>
2021-10-25 15:20:58 +0300

    initial commit

---
+++ src/main/java/org.heaphop/HelloAction.java
@@ -0,0 +1,14 @@
+package org.heaphop;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
+import org.jetbrains.annotations.NotNull;
+
+public class HelloAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Messages.showMessageDialog(e.getProject(), "Hello message","Hello Title", Messages.getInformationIcon());
+    }
+}
